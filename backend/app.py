9cfde51c6893f992de37c8d07062e5e095c32fdd
Harikrishnan Narasiman <harikrish@Harikrishnans-MacBook-Pro.local>
2025-07-22 10:53:45 -0400

    Add port value from environment variable

--- backend/app.py
+++ backend/app.py
@@ -43,4 +43,4 @@ if __name__ == '__main__':
 
     with app.app_context():
         db.create_all()
-    app.run(debug=True, host='0.0.0.0', port=5001) 
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5001)))
